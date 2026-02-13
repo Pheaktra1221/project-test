4d4991f6a21af368a77decdb1914be64b2c0c134
Gintra121 <vannaktra57@gmail.com>
2026-02-13 19:33:31 +0700

    changes

--- server/index.js
+++ server/index.js
@@ -148,7 +148,7 @@ if (fs.existsSync(distPath)) {
   });
 }
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 const HOST = process.env.HOST || '0.0.0.0'
 
 httpServer.listen(PORT, HOST, () => {
